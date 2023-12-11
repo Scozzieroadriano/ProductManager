34789044c87de6befbb81e3a258130da06f4fbd4
Scozzieroadriano <scozzieroadriano@gmail.com>
2023-12-11 16:49:36 -0300

    brypt

--- src/daos/mongodb/user.dao.js
+++ src/daos/mongodb/user.dao.js
@@ -1,4 +1,5 @@
 import { UserModel } from "./models/user.model.js";
+import { hashPassword,isValidPassword } from "../../utils.js";
 
 export default class UserDao {
 
@@ -15,37 +16,44 @@ export default class UserDao {
         try {
             const { email, password } = user;
             const userExists = await this.findUserByEmail(email);
-    
+
             if (userExists) {
                 throw new Error(`Ya existe un usuario con el email: ${email}`);
             } else {
                 if (email === 'adminCoder@coder.com' && password === 'adminCod3r123') user.role = 'admin';
-                const response = await UserModel.create(user);
+                const response = await UserModel.create({ ...user, password: hashPassword(password) });
                 return response;
             }
         } catch (error) {
             throw new Error('Ocurrió un error durante el registro');
         }
     }
-    
+
 
     async login(user) {
         try {
             const { email, password } = user;
-            const userExists = await UserModel.findOne({ email, password });
-    
-            if (!userExists) {
-                const response = ({error:`El email o la contraseña son incorrectos`});
-                return response
+            const userExists = await UserModel.findOne({ email });
+
+            if (userExists) {
+                const isValid = isValidPassword(password,userExists);
+
+                if (!isValid) {
+                    const response = { error: `La contraseña es incorrecta` };
+                    return response;
+                } else {
+                    return userExists;
+                }
+            }else{
+                const response = { error: `No existe un usuario con el email: ${email}` };
+                return response;
             }
-    
-            return userExists;
         } catch (error) {
             // Manejo del error
             console.error(error); // Registrando el error para su seguimiento
-    
+
             throw new Error('Ocurrió un error durante el inicio de sesión'); // Relanzar el error para la gestión global
         }
     }
-    
+
 }
